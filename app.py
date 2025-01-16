97a0bbf386ecbcf43280793137380b490ea59df5
Kamal. <130303828+plebingly@users.noreply.github.com>
2025-01-15 20:48:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,5 @@ def profile():
     return render_template('profile.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
